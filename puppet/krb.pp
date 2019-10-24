981193f30fc186a7f6dc7f3af33788fab433e0f4
Petr Benas <petr.benas@gooddata.com>
2019-10-24 11:05:46 +0200

    Various puppet improvements
    Changed default sound and video, updated user list.

--- puppet/krb.pp
+++ puppet/krb.pp
@@ -22,17 +22,24 @@ class base {
     purge_ssh_keys => true,
   }
 
-  ssh_authorized_key { 'stepan@stepan-ntb':
-    user   => $::cfg::user,
-    ensure => present,
-    key    => 'AAAAB3NzaC1yc2EAAAABIwAAAgEAy2evqVvSAyirplCBRAp5mEv2EB0K71HNEDt0xuJwB1PJoODy+tjJjgIvQkppBIhDnWjgiepxp4ITmrHJ18nqTLbUIZ8mczBhb8BZdIJptbDtP6mZiGxnraYwTlz0a/BAC948rcqf2xrZbTUdd247mPWdGzhXGmhyB/Avn6hCLFDAvhyBm7bDOxmBAa5pD4f4AlMoSdmMVu3BuJHPrFFBSZy6lcKuqC0wywUWhMm3VDD0+g4EM/pNo6aKg9pSYe9W++f6W53Vnfs/WUcCKo8QKT2oSeo5Sdk+pqJJEla4rLPYfjy/+YBGo0BJ95Zj/VAmpEbyhht7kJw7Bw9lbgXW/2euwFuwsALj809HNaXsLWYnnrwar1MxcJRQWyGmt+Wt1fee5NMzSDBsp05S6x6wWxpeh5Ubc0d6fIEsJ3eIhubQOzd6VZk02F8ctc86jwNWapQYgY/RBP8D7u3B6hBUJXWR9ryC+5CrKYVd5FCOnUiDSpk4AyYc26uzLuZL6Zjp+iaBbthVjTzazUmIuvg1tV6I4Pygbx+x6W/nQXpTMwJD+w8oUiFSlllXcHC3e/y6IS084B3KTjIco0n7gyGJvZNzvsQF5WcDqYr4ccocCcXEdGIQy16YDkYITNl03FxQZIiQc+gCTyph+Atqk6ZqbKYmmvR+kGna//Bh5WL9sik=',
-    type   => 'ssh-rsa'
-  }
-
   ssh_authorized_key { 'petr.benas@gooddata.com':
     user   => $::cfg::user,
     ensure => present,
     key    => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQC/dYtN0/kJPAsoKs1LJ9wRP5Sz8AmkF3p/1E1h9RTlrJ3XFlU6Aba/CoIoK/QSPweXQJW9f9ZTCDgt5YnwNHBVuKWoS5Yc48pUK0dnvtLnuVd59C4I7MP0VK3MD1tucM4DQh8m1PRfWGfygtarBwWVR2awwGP5sJsSPppvf1kJAtIaJ3DPxWdkBsEqZTg2hGf6f9oOCtsXr2ml0Zk1XhDO1+ryFiq9F5C7ATNindlMPEo31Ac4yDkVt8p/aK2UeRX2SjcFAGwndL6X04pdC2tQR7BPWCI6qTLuOZ2hsK7dCjjgH5wbHLWGecEmtCpRCQDiQmuGqXTUKwWZ29Nqj8GOCnuQ1omhTQjqy/Qod+H8IFwbFqZ9B6hcYSpIPKWb815D9wtUJglZpvriYMlJ8fohWpkkPsCyaOSL1JIp5GOn+99wbBopI9wyopusBZ2+AyipvnSPFxFYseWL58kv/xuyVKSSGfXE22r75d5QG29+BvnLjgwnMhXlk08jH43FvVGocjXTTgFdNF31cjuYWOZlZMSeNWM7ywFjgQV2EAhkFm1RfXbzSiudzIktWPoDV1P5cq4Ec3hBYYlgt3F4OpD98my9v259E8xWbgZptY4SOwCZZC5M3uy/Tz7NzNUFKLapkTwg+reYztyoXZtn+rKzDnbmDR1+LabRVydN0gAk5w==',
+    type   => 'ssh-rsa'
+  }
+
+  ssh_authorized_key { 'kristian.lesko@gooddata.com':
+    user   => $::cfg::user,
+    ensure => present,
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDCepce4WJp1n+PeMKUCYern2RF/GIbOq39IhgjkdqditfBTNeHCMfYpdy/Nn20SxNgJ1qu3zvEMFmBN9MDZ0u/X/crDeM6LHgi5wpnXiKmQTqKvNvbfmLlH6pgXEg019ObHLR8mxus62lbvuvWocXaQW9OZM8gS6Kes6lG405zccQklZUNvquyXHitJW9za5A1pbZDa4LbBi2qjcUHKL+cREl/fUWjAHbAlMCCIZ0QrEKo9gHGG0qVl2TtKczpIuO4cm9P//fu0FDDvG9JvCJzpcMjBsWWRDjTEMqTV5b4IknmqKgt1cMr88nTx43QwZksi2iRk3cAkDYdo+5U8gwfVvHA/PW8ek+UqtbdYfahl63jXRqw1kD8SxNI7799D17Fe6/FzMffym2ScZgSybtIooZVuUu0DbF+jeewNe0ST63B1VuW9+c5/ZrOkNLmzdiElpVRg8ZptezUxqZSKh8lID+TPev5Qx1gmkn3JWAB16/gruo49tqzecizHN5SQEvNk8Wu5UvAMgeJ9FqHxsM2ZxgKZSZZafVKme4oGQjbMeIxXeYdXVuyK/uYTw9YnUN75ZP7h7Wj44UEoBB35vamjyv3E27/6v9XtwVbO3mEPJUCgci3fU7NI6Cb1CsTmvWD19SyfKQsNYvowZqGyioeBGo+hFXYCK5ch+VYCztXJQ==',
+    type   => 'ssh-rsa'
+  }
+
+  ssh_authorized_key { 'martin.goldammer@gooddata.com':
+    user   => $::cfg::user,
+    ensure => present,
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDWO+2g18unnEIedw3QnwFKVPTqqvzLGbZz6k0qT1385rnN86zbiCqMon+KFYIPnVNPAX4rCeoPjwKu10bgLe39NXL4sgmmfBbxHlZ7AyLV/NT07jBVHeePyNMsj+2WvsKNxIOOtlKNg/z2GDrNeTqwVZbxYbejwoXpcIAm0WPrHjsEUtNuPcYsDGpDzdbL7b3SU0MITF9lX6dcqaijGvW88zxqKBqOZJCz81uxwhnGsJl08FkR91Rc3r+nb5KOAYI690ZrViDQCMbXzbUKrzwxqn1iqKlXNj3vOK6vskeI5Yk9HgjfUBSkzrlLXHBUN5FoHLC5VDmdSzoxSZYNoip16npaf48IYCOKD/GtT7dO1Im2QVMbn0tBX9f0odGECDx1nr+B0/N6aruM/x/l5chu/84diNdgPu0TGR/vZBncpNxYasMmEEx9CiB/VsarZF6K1/r5OxE0Gr2PIZHajcA7nlD8JEZaqJLfzMQ1XE2drGh/ga4wPHR/BzcX3Cd9HFb7pI9eYhet2juiqDgCwKUC17Mljk0r4fGXIyWPh9gRwWQ/tDCn9nyBU9VnSFBUQ1/jWRU5t25ahMGx6NyQ4FYcl0uOYceFaUnJMU6nmwdgYe7FAzR/DILhCUfWiLz2OQn1bh1MZdZktfGZA4w+xAfuGJuKVwdfL/YfFLa+A6FPuQ==',
     type   => 'ssh-rsa'
   }
 }
@@ -61,8 +68,8 @@ LANGUAGE=en_UTF.UTF-8'
 }
 
 class krb {
-  cron { 'skola':
-    command => "/home/${::cfg::user}/fireplace/scripts/play-rand.sh skola",
+  cron { 'non-gdc':
+    command => "/home/${::cfg::user}/fireplace/scripts/play-rand.sh scream",
     hour    => '7-19',
     minute  => '0',
   } ->
@@ -79,7 +86,7 @@ Description=Ohen As AS ervice
 Type=simple
 User=pi
 WorkingDirectory=/home/pi
-ExecStart=/home/pi/fireplace/scripts/play.sh /home/pi/fireplace/movies/ohen.mp4
+ExecStart=/home/pi/fireplace/scripts/play.sh /home/pi/fireplace/movies/ohen2.mp4
 Restart=on-failure
 
 [Install]
